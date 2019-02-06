d71ee345476a8bc0374aef59947df2b6d902e0e1
ZetaChow <zetachow@163.com>
2019-02-07 02:19:14 +0800

    修改代码

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 // Modules to control application life and create native browser window
 const {
     app,
-    BrowserWindow,
-    BrowserView
+    BrowserWindow
 } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
